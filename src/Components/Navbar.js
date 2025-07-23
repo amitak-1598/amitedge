522a89639cdb691965c7527ce62da36439c61bbe
Amit Kumar <amitak3107@gmail.com>
2025-07-23 17:45:03 +0530

    update navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaEnvelope, FaPhoneAlt } from 'react-icons/fa';
 import '../Navbar.css';
 
 const Navbar = () => {
@@ -12,16 +13,21 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <a href="/" className="navbar-logo">AmitEdge</a>
+        <Link to="/" className="navbar-logo">AmitEdge</Link>
 
-      <ul className={`nav-links ${isMobileMenuOpen ? 'active' : ''}`}>
-  <li><Link to="/" className="nav-link">Home</Link></li>
-  <li><Link to="/courses" className="nav-link">Courses</Link></li>
-  <li><Link to="/results" className="nav-link">Results</Link></li>
-  <li><Link to="/about" className="nav-link">About</Link></li>
-  <li><Link to="/contact" className="nav-link">Contact</Link></li>
-  <li><Link to="/join" className="nav-cta">Join Now</Link></li>
-</ul>
+        <ul className={`nav-links ${isMobileMenuOpen ? 'active' : ''}`}>
+          <li><Link to="/" className="nav-link">Home</Link></li>
+          <li><Link to="/courses" className="nav-link">Courses</Link></li>
+          <li><Link to="/results" className="nav-link">Results</Link></li>
+          <li><Link to="/about" className="nav-link">About</Link></li>
+          <li><Link to="/contact" className="nav-link">Contact</Link></li>
+          <li><Link to="/join" className="nav-cta">Join Now</Link></li>
+        </ul>
+
+        <div className="navbar-contact">
+          <span id='icon-1'><FaPhoneAlt className="icon" /> +91 98765 43210</span>
+          <span><FaEnvelope className="icon" /> support@amitedge.com</span>
+        </div>
 
         <button className="menu-btn" onClick={toggleMenu}>
           ☰
